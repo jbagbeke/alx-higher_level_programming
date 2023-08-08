e79d7fc2c52112cfc062292d01f6b1e6839db323
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-08 20:30:09 +0000

    Python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,13 +2,5 @@
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return True
-    if ord(c) >= 65 and ord(c) <= 90:
+    else:
         return False
-
-
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
