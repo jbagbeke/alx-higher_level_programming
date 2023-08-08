7170fb85f71b76c6fc7f4a21c27ad11077967723
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-08 14:16:58 +0000

    Python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-for alpha in range(65, 91):                               print(chr(alpha).lower(), end='')
+for alpha in range(65, 91):                               print(f"{chr(alpha).lower()}", end='')
