d331de7fac31781bd4740702560012c99c2d4376
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-08 21:06:59 +0000

    Python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+upper_letter = ''
+for letter in test:
+    if 'a' <= letter <= 'z':
+        letter = chr(ord(letter) - 32)
+        upper_letter += letter
+    else:
+        upper_letter += letter
+print(str)
