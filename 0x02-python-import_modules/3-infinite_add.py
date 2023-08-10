6d6685a8d5df9508e2b5d5384c79d7ea11e21a81
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-10 22:29:34 +0000

    Python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-arg_len = len(argv)
-total_sum = 0
-if arg_len < 2:
-    print("{}".format(0))
-if arg_len == 2:
-    print("{}".format(argv[1]))
-if arg_len > 2:
-    for i in range(arg_len - 1):
-        total_sum += int(argv[i + 1])
-    print("{}".format(total_sum))
+if __name__ == "__main__":
+    arg_len = len(argv)
+    total_sum = 0
+    if arg_len < 2:
+        print("{}".format(0))
+    if arg_len == 2:
+        print("{}".format(argv[1]))
+    if arg_len > 2:
+        for i in range(arg_len - 1):
+            total_sum += int(argv[i + 1])
+        print("{}".format(total_sum))
