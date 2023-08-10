5c77d2dc8d9a35ac60cf0abba25f248b7c49767a
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-10 19:04:13 +0000

    Python

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,11 +2,14 @@
 def add_it(a, b):
     return (a + b)
 
+
 def sub_it(a, b):
     return (a - b)
+
 
 def div_it(a, b):
     return (a / b)
 
+
 def mul_it(a, b):
-    return(a * b)
+    return (a * b)
