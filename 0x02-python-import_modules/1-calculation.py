3b17ba5f325441d12d28cb3179cd6b989576f39c
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-10 20:50:56 +0000

    Python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add_it, mul_it, sub_it, div_it
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, add_it(a, b)))
-print("{} - {} = {}".format(a, b, sub_it(a, b)))
-print("{} / {} = {}".format(a, b, div_it(a, b)))
-print("{} * {} = {}".format(a, b, mul_it(a, b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add_it(a, b)))
+    print("{} - {} = {}".format(a, b, sub_it(a, b)))
+    print("{} / {} = {}".format(a, b, div_it(a, b)))
+    print("{} * {} = {}".format(a, b, mul_it(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-arg_len = len(argv)
-if arg_len < 2:
-    print("{} arguments.".format(0))
-if arg_len == 2:
-    print("{} argument:\n{}: {}".format(1, 1, argv[1]))
-if arg_len > 2:
-    print("{} arguments:".format(arg_len - 1))
-    for i in range(arg_len - 1):
-        print("{}: {}".format(i+1, argv[i+1]))
+if __name__ == "__main__":
+    arg_len = len(argv)
+    if arg_len < 2:
+        print("{} arguments.".format(0))
+    if arg_len == 2:
+        print("{} argument:\n{}: {}".format(1, 1, argv[1]))
+    if arg_len > 2:
+        print("{} arguments:".format(arg_len - 1))
+        for i in range(arg_len - 1):
+            print("{}: {}".format(i+1, argv[i+1]))
