870af9a08bb03d9e1217b58c44f35b9a383c8993
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-10 22:25:12 +0000

    Python

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 def add(a, b):
-    a = 10
-    b = 5
     return (a + b)
 
 
 def sub(a, b):
-    a = 10
-    b = 5
     return (a - b)
 
 
-def div(a, b):
-    a = 10
-    b = 5
-    return (a / b)
-
-
 def mul(a, b):
-    a = 10
-    b = 5
     return (a * b)
+
+
+def div(a, b):
+    return (a / b)
