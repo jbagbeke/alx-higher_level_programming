92dffcc57fc736e8e180c0bd35e35086705732ab
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-10 21:51:19 +0000

    Python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,13 @@ from calculator_1 import add_it, mul_it, sub_it, div_it
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add_it(a, b)))
-    print("{} - {} = {}".format(a, b, sub_it(a, b)))
-    print("{} / {} = {}".format(a, b, div_it(a, b)))
-    print("{} * {} = {}".format(a, b, mul_it(a, b)))
+
+    add_result = add_it(a, b)
+    sub_result = sub_it(a, b)
+    div_result = div_it(a, b)
+    mul_result = mul_it(a, b)
+
+    print("{} + {} = {}".format(a, b, add_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} / {} = {}".format(a, b, div_result))
+    print("{} * {} = {}".format(a, b, mul_result))
