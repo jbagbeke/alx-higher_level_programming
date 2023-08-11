7473c4eb0e5bc0fafcdafde5c756f59fa3067c81
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-11 00:44:58 +0000

    Python

--- 0x02-python-import_modules/first_calculator.py
+++ 0x02-python-import_modules/first_calculator.py
@@ -2,12 +2,16 @@
 from sys import argv
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+    
     arg_len = len(argv)
+    
     usage = "Usage: ./100-my_calculator.py <a> <operator> <b>"
     operators = "Unknown operator. Available operators: +, -, * and /"
+    
     if (arg_len - 1) < 3:
         print(usage)
         exit(1)
+    
     if (arg_len - 1) >= 3:
         opcode = 2
         for opcode in range(opcode, arg_len - 1, 3):
