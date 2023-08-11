2ab85153ff98eaa6282934c7854c3898078bafe6
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-11 00:15:56 +0000

    Python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,29 +5,33 @@ if __name__ == "__main__":
     arg_len = len(argv)
     usage = "Usage: ./100-my_calculator.py <a> <operator> <b>"
     operators = "Unknown operator. Available operators: +, -, * and /"
-    if (arg_len - 1) < 3:
+    if (arg_len - 1) != 3:
         print(usage)
         exit(1)
+    
     if (arg_len - 1) >= 3:
         opcode = 2
         for opcode in range(opcode, arg_len - 1, 3):
-            match argv[opcode]:
-                case '+':
-                    a = int(argv[opcode - 1])
-                    b = int(argv[opcode + 1])
-                    print("{} + {} = {}".format(a, b, add(a, b)))
-                case '-':
-                    a = int(argv[opcode - 1])
-                    b = int(argv[opcode + 1])
-                    print("{} - {} = {}".format(a, b, sub(a, b)))
-                case '*':
-                    a = int(argv[opcode - 1])
-                    b = int(argv[opcode + 1])
-                    print("{} * {} = {}".format(a, b, mul(a, b)))
-                case '/':
-                    a = int(argv[opcode - 1])
-                    b = int(argv[opcode + 1])
-                    print("{} / {} = {}".format(a, b, div(a, b)))
-                case _:
-                    print(operators)
-                    exit(1)
+            if argv[opcode] == '+':
+                a = int(argv[opcode - 1])
+                b = int(argv[opcode + 1])
+                print("{} + {} = {}".format(a, b, add(a, b)))
+                continue
+            if argv[opcode] == '-':
+                a = int(argv[opcode - 1])
+                b = int(argv[opcode + 1])
+                print("{} - {} = {}".format(a, b, sub(a, b)))
+                continue
+            if argv[opcode] == '*':
+                a = int(argv[opcode - 1])
+                b = int(argv[opcode + 1])
+                print("{} * {} = {}".format(a, b, mul(a, b)))
+                continue
+            if argv[opcode] == '/':
+                a = int(argv[opcode - 1])
+                b = int(argv[opcode + 1])
+                print("{} / {} = {}".format(a, b, div(a, b)))
+                continue
+            else:
+                print(operators)
+                exit(1)
