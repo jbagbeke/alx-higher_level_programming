33fd5709dcc63b60b24f0e7c026157e27dc9bbff
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 22:04:33 +0000

    Python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix)):
             if isinstance(matrix[i], list):
                 for j in matrix[i]:
-                    print("{:d}".format(j), end=" ")
+                    print("{:d}".format(j), end=" " if j != i[-1] else "")
                 print()
