3d1b8ec3e0914034907c6f5e3eca2cc54217c5a6
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 17:06:41 +0000

    Python

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for num in range(len(my_list)):
-            print("{:d}".format(my_list[num]))
+def print_list_integer(my_list=[]):
+    for num in range(len(my_list)):
+        print("{:d}".format(my_list[num]))
