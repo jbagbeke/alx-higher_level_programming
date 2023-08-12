79dcd1e85ed703a53310d1757dc45ee882d6e683
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 20:57:17 +0000

    Python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,7 +8,3 @@ def no_c(my_string):
             else:
                 new_string += index
         return (new_string)
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
