855acb12931e5acd561aac008a7a1bd122ec4fd2
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 20:10:12 +0000

    Python

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    for index in range(len(my_list)):
+        if index == idx:
+            new_list = my_list.copy()
+            new_list[index] = element
+            return (new_list)
