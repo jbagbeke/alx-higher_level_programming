d3aeb0223c3df3bdcf892dc87cdc4dde0332b130
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 21:57:36 +0000

    Python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in matrix[i]:
-            print("{:d}".format(j), end=" ")
-        print()
+    if isinstance(matrix, list):
+        for i in range(len(matrix)):
+            if isinstance(matrix[i], list):
+                for j in matrix[i]:
+                    print("{:d}".format(j), end=" ")
+                print()
