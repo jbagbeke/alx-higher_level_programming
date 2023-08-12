e9fc953a82fc8fcf2774e2206a25f233236127ba
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-12 17:18:19 +0000

    Python

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (None)
+
+    for index in range(len(my_list)):
+        if index == idx:
+            return(my_list[index])
