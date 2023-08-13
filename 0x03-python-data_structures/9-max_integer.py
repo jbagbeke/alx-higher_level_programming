b804993f6e83fbb8af23d3ac3c76ea663e055498
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-13 01:05:34 +0000

    Python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+def max_integer(my_list=[]):
+    if isinstance(my_list, list):
+        list_len = len(my_list)
+        if list_len == 0:
+            return (None)
+
+        max_int = 0
+        for index in (range(list_len)):
+            if max_int > my_list[index]:
+                continue
+            else:
+                max_int = my_list[index]
+        return (max_int)
