d774dd2f9f54cf057ccede3e705d935217923a69
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 16:50:43 +0000

    Python

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) and value != None:
         if key in a_dictionary:
             a_dictionary[key] = value
         if key not in a_dictionary:
