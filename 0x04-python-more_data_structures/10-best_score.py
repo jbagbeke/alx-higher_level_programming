595bfaea4eb2623c5c6cd80fc1dffae26ef0e9df
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 18:36:00 +0000

    Python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+
     if isinstance(a_dictionary, dict):
         values = max(a_dictionary.values())
         for val in a_dictionary:
