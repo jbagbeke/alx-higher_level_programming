5cbd827eb14935e6e5ac24634b623aed328fe986
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 14:30:13 +0000

    Python

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -11,9 +11,3 @@ def only_diff_elements(set_1, set_2):
                     diff_list.append(idx_2)
         
         return (set(diff_list))
-
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
