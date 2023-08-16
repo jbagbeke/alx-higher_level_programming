b5cce10c4f70dcfd4e83fc903e873fd1404c0b0c
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 16:12:14 +0000

    Python

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        sorted_key = sorted(a_dictionary.keys())
+
+        for key in sorted_key:
+            print("{}: {}".format(key, a_dictionary[key]))
+
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
