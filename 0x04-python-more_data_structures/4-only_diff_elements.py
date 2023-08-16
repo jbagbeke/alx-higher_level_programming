f8392f821eff02d689cc93e4ec357aa4baf9233f
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 15:26:05 +0000

    Python

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,10 +4,11 @@ def only_diff_elements(set_1, set_2):
     if isinstance(set_1, set) and isinstance(set_2, set):
         diff_list = []
         for idx_1 in set_1:
-            for idx_2 in set_2:
-                if idx_1 not in set_2 and idx_1 not in diff_list:
-                    diff_list.append(idx_1)
-                if idx_2 not in set_1 and idx_2 not in diff_list:
-                    diff_list.append(idx_2)
+            if idx_1 not in set_2 and idx_1 not in diff_list:
+                diff_list.append(idx_1)
+
+        for idx_2 in set_2:
+            if idx_2 not in set_1 and idx_2 not in diff_list:
+                diff_list.append(idx_2)
 
         return (set(diff_list))
