6b041ead038371c58286c187c82b26e1faa6a496
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-16 14:12:43 +0000

    Python

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -8,8 +8,3 @@ def common_elements(set_1, set_2):
                 if idx_1 == idx_2 and idx_1 not in com_list:
                     com_list.append(idx_1)
         return (set(com_list))
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
