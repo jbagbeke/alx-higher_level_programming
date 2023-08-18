6db930822e3834337eb5d6a242fc5482ce47e698
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 22:03:36 +0000

    Python

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    if isinstance(a_dictionary, dict):
+        dic_len = len(a_dictionary)
+        del_list = []
+        for key in a_dictionary:
+            if a_dictionary[key] == value:
+                del_list.append(key)
+        for key_del in del_list:
+            del(a_dictionary[key_del])
+        del_list[:] = []
+        return (a_dictionary)
