c3d61b9477a3a0a073700f9b1d9a170b12f98603
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-28 20:07:08 +0000

    PythonMe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(my_list[n]), end='')
                 index += 1
             except IndexError:
-                break
-        print('')
+                pass
         return (index)
