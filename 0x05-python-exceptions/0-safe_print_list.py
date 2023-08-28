4d2ac4ef7fdef7994ca35834b04e11e5bf38a976
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-28 19:44:38 +0000

    PythonIT

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    if isinstance(my_list, list):
+        index = 0
+        length = 0
+        for i in my_list:
+            length += 1
+        for n in range(x):
+            try:
+                print("{}".format(my_list[n]), end='\n' if n == (length - 1) else '')
+            except IndexError:
+                pass
+            else:
+                index += 1
+        return (index)
---
+++ 0x05-python-exceptions/t.py
@@ -0,0 +1,5 @@
+
+mlist = [33, 45, 56, 45, 696]
+
+for n in range(5):
+    print("{:d}".format(mlist[n]), end='' if n )
