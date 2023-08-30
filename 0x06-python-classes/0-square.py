6edec827e4efa6d7ad34eec9b581e0417b489f12
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-30 20:55:20 +0000

    Class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Purpose: An empty class 'Square' that defines a Square
+"""
+class Square:
+    """
+    A class for defining a square
+    """
+    def square(self):
+        """
+        An empty definition of an instance of square
+        """
