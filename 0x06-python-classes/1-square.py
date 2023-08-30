37d91debcf7e1c51dd059b5626d29cce965fd7fa
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-30 21:31:47 +0000

    Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Purpose: A definition of a class 'Square' with a private instance 'size'
+"""
+
+
+class Square:
+    """
+    Class with an instantiation attribut 'size'.
+    """
+    def __init___(self):
+        """
+        Instance definition of the class Square with private attribute 'size'.
+        """
+        self.__size = None
