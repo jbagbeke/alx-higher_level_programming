a84a7ea502b5ed55038113abdf46c8b0a5f8c3e1
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-31 00:42:31 +0000

    Class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,18 +27,16 @@ class Square:
         retrieved = self.__size
         return retrieved
 
-
     def self(self, value):
-        """                                               Instantiation with private 'size' attribute, and checks for valid input
+        """
+        Instantiation and checks for valid input
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-    
         if value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
 
     def area(self):
         """
