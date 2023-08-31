48742539d101bd4d3ef2dea34d51aed03832a445
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-31 00:46:38 +0000

    Class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,14 +11,10 @@ class Square:
     def __init__(self, size=0):
         """
         Instantiation with private 'size' attribute, and checks for valid input
+
+        Args: An int for assignment
         """
-        self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     def size(self):
         """
@@ -31,6 +27,7 @@ class Square:
         """
         Instantiation and checks for valid input
         """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
