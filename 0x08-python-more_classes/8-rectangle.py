a91e4df3b70967cf596d23b06e7187280fafddce
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-05 02:20:40 +0000

    JBA_ALX

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -111,7 +111,6 @@ class Rectangle:
             Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    @staticmethod_
     def bigger_or_equal(rect_1, rect_2):
         """
         Returns biggest rectangle based on area
