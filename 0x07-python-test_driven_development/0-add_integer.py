b1fca37a177753559d1cbb8c1d8a7687e3682074
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-09-08 21:39:12 +0000

    We go

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,13 +14,10 @@ def add_integer(a, b=98):
 
     Returns: Result of summation(Int)
     """
-    max_len = 1e-10
-    if not isinstance(a, int) and isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(a, int) and isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    if (a + 1 - a) < max_len or (b + 1 - b) < max_len:
-        raise ValueError("Integer value too large")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
