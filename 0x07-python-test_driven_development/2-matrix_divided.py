fe346f3bd780222fa2c816542095c8c756b5a9d0
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-09 23:22:12 +0000

    ALX_ME

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,11 +7,11 @@ Divides all elements of a matrix
 def matrix_divided(matrix, div):
     """
     Divides each element of a matrix and rounds to 2 d.p
-    
+
     Args:
         Arg1: A matrix with a list of lists(matrix)
         Arg2: What to divide each element with(div)
-    
+
     Returns:
         A new matrix with result of division
     """
@@ -20,22 +20,24 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     if div == 0:
-        raise ZeroDivisionError("division by zero")    
+        raise ZeroDivisionError("division by zero")
 
     if isinstance(matrix, list):
         for mat1 in matrix:
             if not isinstance(mat1, list):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                t_err = "matrix must be a matrix (list of lists) of integers/floats" 
+                raise TypeError(t_err)
             for is_int in mat1:
                 if not isinstance(is_int, int) and not isinstance(is_int, float):
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError(t_err)
 
         for i in range(len(matrix)):
             if i + 1 < len(matrix):
                 len1 = len(matrix[i])
                 len2 = len(matrix[i + 1])
                 if len1 != len2:
-                    raise TypeError("Each row of the matrix must have the same size")
+                    t_err = "Each row of the matrix must have the same size"
+                    raise TypeError(t_err)
 
     new_matrix = []
     for mat_rix in matrix:
