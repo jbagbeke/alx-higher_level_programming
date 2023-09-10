2dd0948df34605987c1cc1bafeea971c2b7bdbef
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-10 00:25:20 +0000

    ALX_ME

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -22,3 +22,8 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
