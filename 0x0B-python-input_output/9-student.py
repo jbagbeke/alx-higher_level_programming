6b334931594bcac8c9b4a2db5704eb0fcdf73732
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 15:35:56 -0700

    ALX_AND_ME

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Defines a student class with public instance variables
+"""
+
+
+class Student:
+    """
+    Instantiates student class with instance vars
+    """
+
+    def __init__(self, first_name, last_name, age):
+    """
+    Student public method with instantiation
+    """
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    def to_json(self):
+    """
+    Returns the dictionary description
+    """
+
+    return (self.__dict__)
