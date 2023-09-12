25161a4b6744a63860b26d540911dc9bbe39343e
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 10:37:05 -0700

    ALX_AND_ME

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Returns JSON representation of object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns object representation in JSON
+
+    Args:
+        Arg1: (my_obj) object to serialize
+
+    Return: JSON rep of my_obj
+    """
+
+    return (pickle.dumps(my_obj))
