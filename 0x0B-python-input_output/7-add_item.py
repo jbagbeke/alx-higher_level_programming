05b5fa4df12bcd8962a0f2802e0010031e8faca6
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 14:58:54 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,16 @@ A script that adds all arguments to a Python list
 
 
 import sys
-import os
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if not os.path.exists("add_item.json"):
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
     jba_list = []
     with open("add_item.json", "w", encoding="UTF-8") as file:
         json.dump(jba_list, file)
-else:
-    my_list = load_from_json_file("add_item.json")
-    for i in range(len(sys.argv) - 1):
-        my_list.append(sys.argv[i + 1])
+for i in range(len(sys.argv) - 1):
+    my_list.append(sys.argv[i + 1])
 save_to_json_file(my_list, "add_item.json")
