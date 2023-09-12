ea87a294469cbce8a58fd298f8ccf32e85d05904
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 15:42:29 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,17 +10,17 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-    """
-    Student public method with instantiation
-    """
+        """
+        Student public method with instantiation
+        """
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-    """
-    Returns the dictionary description
-    """
+        """
+        Returns the dictionary description
+        """
 
-    return (self.__dict__)
+        return (self.__dict__)
