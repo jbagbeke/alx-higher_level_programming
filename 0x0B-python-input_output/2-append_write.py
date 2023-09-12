74fcb886d9e58f3b17066d2ff5120fba1734cffb
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 10:26:30 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@ Appends a string to end of text file
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
     Appends a string to end of text file and returns # of chars added
 
