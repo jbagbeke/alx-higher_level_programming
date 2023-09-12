b235da325420726a9634570658315b4d2b42b007
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 14:14:05 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,10 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 jba_list = []
 
 with open("add_item.json", "w", encoding="UTF-8") as file:
-        json.dump(jba_list, file)
+    json.dump(jba_list, file)
 
 my_list = load_from_json_file("add_item.json")
 
