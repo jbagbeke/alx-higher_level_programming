c2febb5bba2175024921032ca7623a2a2ea48ec4
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 15:11:52 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,14 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    jba_list = []
-    with open("add_item.json", "w", encoding="UTF-8") as file:
+
+jba_list = []
+with open("add_item.json", "w", encoding="UTF-8") as file:
         json.dump(jba_list, file)
-my_list.extend(sys.argv[1:])
+
+my_list = load_from_json_file("add_item.json")
+
+for i in range(len(sys.argv) - 1):
+    my_list.append(sys.argv[i + 1])
+
 save_to_json_file(my_list, "add_item.json")
