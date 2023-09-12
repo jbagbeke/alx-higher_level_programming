b10b416bcfcbfc5a861fa09f26d687d99c84122b
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-12 13:55:57 -0700

    ALX_AND_ME

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,15 +9,17 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+if len(sys.argv) == 1:
+    sys.exit()
+else:
+    jba_list = []
 
-jba_list = []
+    with open("add_item.json", "w", encoding="UTF-8") as file:
+        json.dump(jba_list, file)
 
-with open("add_item.json", "w", encoding="UTF-8") as file:
-    json.dump(jba_list, file)
+    my_list = load_from_json_file("add_item.json")
 
-my_list = load_from_json_file("add_item.json")
+    for i in range(len(sys.argv) - 1):
+        my_list.append(sys.argv[i + 1])
 
-for i in range(len(sys.argv) - 1):
-    my_list.append(sys.argv[i + 1])
-
-save_to_json_file(my_list, "add_item.json")
+    save_to_json_file(my_list, "add_item.json")
