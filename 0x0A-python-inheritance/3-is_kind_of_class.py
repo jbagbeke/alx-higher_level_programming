d1a631bcf36307db4824074a3db1d7ecd056d285
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-11 17:58:38 -0700

    ALX_AND_ME

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -15,7 +15,7 @@ def is_same_class(obj, a_class):
     Return: True if obj is an instance, False otherwise
     """
 
-    if type(obj) == a_class or type(obj) == (a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
