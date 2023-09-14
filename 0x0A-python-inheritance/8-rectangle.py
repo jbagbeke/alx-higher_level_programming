dae33f8f887df4e0faa82b93af792e650fbf2bd1
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-14 13:44:54 -0700

    ALX_AND_ME

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,17 +9,6 @@ class BaseGeometry:
     Class with geometry attributes
     """
 
-    def __init__(self, width, height):
-        """
-        Instantiation with width and height
-        """
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
     def area(self):
         """
         Raises an exception
@@ -36,3 +25,20 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    Class that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Instantiation with width and height
+        """
+
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
