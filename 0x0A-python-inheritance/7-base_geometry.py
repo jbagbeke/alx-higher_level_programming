271448de0b2ffe01ebbfbea4f0d35680d9c0ebf7
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-14 13:04:47 -0700

    ALX_AND_ME

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,6 +22,6 @@ class BaseGeometry:
         """
 
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(value))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(value))
+            raise ValueError("{} must be greater than 0".format(name))
