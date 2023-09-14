ddc32150e9c16261b9a5a399db05b72b086bf8ec
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-14 14:53:37 -0700

    ALX_AND_ME

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -69,6 +69,6 @@ class Square(Rectangle):
         Instantiation with size
         """
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
