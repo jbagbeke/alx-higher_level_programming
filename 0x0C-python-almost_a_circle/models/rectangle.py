3d930aab819ba97edc1f78273b15a9c4e3c9951d
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-18 14:53:53 -0700

    ALX_AND_ME

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -189,21 +189,3 @@ class Rectangle(Base):
         my_dict['height'] = self.height
         my_dict['width'] = self.width
         return my_dict
-
-
-
-
-
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2, 1, 9)
-    print(r1)
-    r1_dictionary = r1.to_dictionary()
-    print(r1_dictionary)
-    print(type(r1_dictionary))
-
-    r2 = Rectangle(1, 1)
-    print(r2)
-    r2.update(**r1_dictionary)
-    print(r2)
-    print(r1 == r2)
