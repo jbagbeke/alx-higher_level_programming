9726798281ebbd171ecd4e52bdbc39c20eccc571
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-18 17:32:20 -0700

    ALX_AND_ME

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,3 +18,18 @@ class BaseTest(unittest.TestCase):
 
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+    def test_One_Arg(self):
+        """
+        Passing one arg to Base
+        """
+
+        b1 = Base(34)
+        self.assertEqual(b1.id, 34)
+        b2 = Base(940)
+        self.assertEqual(b2.id, 940)
+        b3 = Base(1)
+        self.assertEqual(b3.id, 1)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unittest for module base.py
+Unittest for module rectangle.py
 """
 import unittest
 from models.rectangle import Rectangle
@@ -8,13 +8,53 @@ from models.rectangle import Rectangle
 
 class RectangleTest(unittest.TestCase):
     """
-    Unittest for Base class
+    Unittest for Rectangle class
     """
 
-    def test_all_args(self):
-        """
-        Passing None Argument to Base class
-        """
+    def test_no_arg(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
 
-        r1 = Rectangle(3, 2)
-        self.assertEqual(r1.area(), 6)
+    def test_one_arg(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(2)
+
+    def test_two_args(self):
+        r1 = Rectangle(6, 2)
+        r2 = Rectangle(2, 3)
+        self.assertEqual(r2.id, r1.id + 1)
+
+    def test_three_args(self):
+        r1 = Rectangle(1, 2, 3)
+        r2 = Rectangle(4, 5, 6)
+        self.assertEqual(r1.id, r2.id - 1)
+
+    def test_four_args(self):
+        r1 = Rectangle(2, 3, 4, 5)
+        r2 = Rectangle(34, 5, 2, 5)
+        self.assertEqual(r1.id, r2.id - 1)
+
+    def test_all_args(self):
+        r1 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 12)
+
+    def test_height_int(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.height = "2"
+
+    def test_x_int(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.x = {}
+
+    def test_width_less(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 3)
+            r.width = -12
+
+
+    def test_y_less(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2, 3, 1)
+            r.y = -1
