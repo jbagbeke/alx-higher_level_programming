038f2e583122be73279723fa092b255b53e5c151
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-10-17 23:22:13 +0000

    Sql tasks

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 --Group/sort by score
 
-SELECT score, COUNT(*) as number
-FROM hbtn_0c_0.second_table GROUP BY score
+SELECT score, COUNT(*) AS number
+FROM second_table GROUP BY score
 ORDER BY number DESC;
