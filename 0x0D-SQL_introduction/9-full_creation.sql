b4b073049e4d07b0f84d04ebc645ce45836413e2
Joshua Bubune <joshua.legithive@gmail.com>
2023-10-18 01:59:03 +0000

    sql tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,10 +2,12 @@
 CREATE TABLE IF NOT EXISTS second_table(
 	       id INT,
 	       name VARCHAR(256),
-	       score INT
-);
+	       score INT);
 INSERT INTO second_table(id, name, score)
-VALUES(1, "john", 10),
-      (2, "Alex", 3),
-      (3, "Bob", 14),
-      (4, "George", 8);
+VALUES(1, "john", 10);
+INSERT INTO second_table(id, name, score)
+VALUES(2, "Alex", 3);
+INSERT INTO second_table(id, name, score)
+VALUES(3, "Bob", 14);
+INSERT INTO second_table(id, name, score)
+VALUES(4, "George", 8);
