cf912817322413a44cb54abae4bda7a10ba94b64
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-13 20:49:27 +0000

    Javascript warmup task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let allLine = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const allLine = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index = 0;
 
 while (index < 3) {
