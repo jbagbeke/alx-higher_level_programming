4384c350520a27c87bc3f679e9c3702336486d70
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-13 21:29:45 +0000

    Javascript warmup task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let a = process.argv[2];
-let b = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 
 function add (a, b) {
   console.log(parseInt(a) + parseInt(b));
