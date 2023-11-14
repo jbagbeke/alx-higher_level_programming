2da61f4324e381369474d80769db79ef368fb423
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-14 02:49:26 +0000

    Javascript warmup task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
 
-let newCommands = process.argv.slice(2);
-let parsedArray;
-let firstMax;
+const newCommands = process.argv.slice(2);
 let index = 0;
 let newArray = [];
 
-parsedArray = newCommands.map(arg => parseInt(arg));
-firstMax = Math.max.apply(null, parsedArray);
+const parsedArray = newCommands.map(arg => parseInt(arg));
+const firstMax = Math.max.apply(null, parsedArray);
 
 if (parsedArray.length < 2) {
   console.log(0);
 } else {
   while (index < parsedArray.length) {
-
     if (parsedArray[index] !== firstMax) {
       newArray[index] = parsedArray[index];
     }
