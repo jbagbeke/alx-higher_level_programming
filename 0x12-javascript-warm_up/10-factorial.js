b5006b478c76521e35270bc75b0199f43ba1c545
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-14 01:03:54 +0000

    Javascript warmup task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
 function factorial (number) {
-  let factorialNum;
   let newNum;
-  let abcd;
 
   if (number === 0 || number === 1 || !number) {
     return (1);
   }
 
   newNum = number;
-  factorialNum = factorial(--number);
+  const factorialNum = factorial(--number);
   newNum = newNum * factorialNum;
 
   return (newNum);
 }
 
-const factNumber = factorial(parseInt(process.argv[2]));
+const factNumber = factorial(process.argv[2]);
 console.log(factNumber);
