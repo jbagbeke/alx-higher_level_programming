97c64fe2d62d397df1cca1e0acd03dddf13144de
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-14 21:30:28 +0000

    JS Objects, Scopes and Classes task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     let idx = 0;
-
     while (idx < this.height) {
       console.log('X'.repeat(this.width));
       idx++;
