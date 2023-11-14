f8095b326d56a2fba8ea61f25dbb4ee8109a9083
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-14 21:56:27 +0000

    JS Objects, Scopes and Classes task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let idx = 0;
+    while (idx < this.height) {
+      console.log('X'.repeat(this.width));
+      idx++;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
