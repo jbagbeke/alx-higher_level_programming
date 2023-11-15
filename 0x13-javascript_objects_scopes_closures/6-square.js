0e1fc86399240ec278884fd99dd5c5f0e4a45712
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-15 00:16:30 +0000

    JS Objects, Scopes and Classes task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Square = require('./5-square.js');
+
+class SquareTwo extends Square {
+  charPrint (c) {
+    if (c) {
+      let idx = 0;
+      while (idx < this.height) {
+        console.log('C'.repeat(this.width));
+        idx++;
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = SquareTwo;
