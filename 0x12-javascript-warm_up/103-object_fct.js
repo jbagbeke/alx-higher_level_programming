bf29a739b14a6a0a788a0030c1fe6a718ded10ca
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-16 02:29:05 +0000

    JS warmup advanced task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,7 +5,9 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => myObject.value = myObject.value + 1;
+myObject.incr = function () {
+  myObject.value = myObject.value + 1;
+};
 
 myObject.incr();
 console.log(myObject);
