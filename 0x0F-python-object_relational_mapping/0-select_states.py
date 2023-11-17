3be2777758884d9678fad725995e7953fe14015f
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-17 19:56:43 +0000

    PYTHON ORM MANDATORY TASK 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,11 @@ import sys
 
 if __name__ == "__main__":
     if (len(sys.argv) == 4):
-        db_connect = MySQLdb.connect(host='localhost', port=3306,  user=sys.argv[1], 
-                passwd=sys.argv[2], db=sys.argv[3])
+        db_connect = MySQLdb.connect(host='localhost',
+                                     port=3306,
+                                     user=sys.argv[1],
+                                     passwd=sys.argv[2],
+                                     db=sys.argv[3])
 
         cursor = db_connect.cursor()
         cursor.execute("SELECT * FROM states ORDER BY states.id")
@@ -15,7 +18,7 @@ if __name__ == "__main__":
         results = cursor.fetchall()
 
         for result in results:
-          print("{}".format(result))
+            print("{}".format(result))
 
         cursor.close()
         db_connect.close()
