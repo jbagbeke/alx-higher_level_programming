33304547cc68b24dbb2782be7b7f7ffb85998e29
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-18 22:02:46 +0000

    ALX ORM MANDATORY TASK 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,18 +4,19 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db_connect = MySQLdb.connect(host='localhost',
-                                 port=3306,
-                                 user=sys.argv[1],
-                                 passwd=sys.argv[2],
-                                 db=sys.argv[3])
-    cursor = db_connect.cursor()
-    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    if (len(sys.argv) == 4):
+        db_connect = MySQLdb.connect(host='localhost',
+                                     port=3306,
+                                     user=sys.argv[1],
+                                     passwd=sys.argv[2],
+                                     db=sys.argv[3])
+        cursor = db_connect.cursor()
+        cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
 
-    results = cursor.fetchall()
+        results = cursor.fetchall()
 
-    for result in results:
-        print(result)
+        for result in results:
+            print(result)
 
-    cursor.close()
-    db_connect.close()
+        cursor.close()
+        db_connect.close()
