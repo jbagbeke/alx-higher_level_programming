611612f4de74b3f96496969df6d3ad4f0e7e48dd
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-18 22:21:01 +0000

    ALX ORM MANDATORY TASK 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
                                      passwd=sys.argv[2],
                                      db=sys.argv[3])
         cursor = db_connect.cursor()
-        cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+        cmd = "SELECT * FROM cities JOIN states ON cities.state_id = states.id"
+        cursor.execute(cmd + " ORDER BY cities.id ASC")
 
         results = cursor.fetchall()
 
