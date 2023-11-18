c82d419d9a3834c06fdef3121afe1b6102347174
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-18 23:08:47 +0000

    ALX ORM MANDATORY TASK 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Lists all cities of that state, using the database hbtn_0e_4_usa """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db_connect = MySQLdb.connect(host='localhost',
+                                 port=3306,
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3])
+    cursor = db_connect.cursor()
+
+    cmd = "SELECT cities.name FROM cities JOIN states ON cities.state_id "
+    cmd1 = "= states.id AND states.name = %s ORDER BY cities.id ASC"
+    cursor.execute(cmd + cmd1, (sys.argv[4].encode(),))
+
+    results = cursor.fetchall()
+    
+    lth = len(results)
+    for result in results:
+        print("{}".format(result[0]), end=', ' if lth > 1 else '\n')
+        lth -= 1
+
+    cursor.close()
+    db_connect.close()
