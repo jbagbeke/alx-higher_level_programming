d878caa8eb0f595e87118171891c28b14b7b2ff2
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-19 18:07:31 +0000

    ALX ORM MANDATORY TASK 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Adds the State object “Louisiana” to the database hbtn_0e_6_usa """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    db_connect = "mysql://{}:{}@localhost:3306/{}".format(sys.argv[1],
+                                                          sys.argv[2],
+                                                          sys.argv[3])
+    engine = create_engine(db_connect)
+    session1 = Session(engine)
+    new_obj = State(name = 'Louisiana')
+
+    session1.add(new_obj)
+    session1.commit()
+
+    result = session1.query(State).filter(State.name == 'Louisiana').all()
+
+    for res in result:
+        print(res.id)
+
+    session1.close()
