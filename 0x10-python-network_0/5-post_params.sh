692aa668e8795c0dcb7be0b506515c9e9ea69320
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-30 22:50:14 +0000

    Python Network mandatory task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends POST request to URL, and displays the body of response
-curl -L -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" $1
+curl -L -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -s $1
