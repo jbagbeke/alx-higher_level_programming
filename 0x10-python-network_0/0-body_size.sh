7130c59226c96932797989f8357cb7996ab2a077
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-30 16:36:06 +0000

    Python Network task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response
-
-request=$(curl -I -s $1)
-
-length=$(echo "$request" | grep -i 'Content-Length' | awk '{print $2}')
-
-echo "$length"
+echo "$(curl -I -s $1 | grep -i 'Content-Length' | awk '{print $2}')"
