0e2f74fd8296c4737a63b2789cd096251607df86
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-30 17:36:42 +0000

    Python Network task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of the response
-if [[ $(curl -w "%{http_code}" -s -o /dev/null $1) -eq 200 ]]; then curl $1; fi
+if [[ $(curl -I -s $1 | grep -i 'HTTP/1.1' | awk '{print $2}') -eq 200 ]]; then curl -I -s $1; fi
