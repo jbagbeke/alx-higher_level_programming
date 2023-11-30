836e8a6131dcbb767b0a612318c092815784f1ce
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-30 22:32:06 +0000

    Python Network mandatory task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -I -L -s -X OPTIONS $1 | grep -i 'allow' | awk -F: '{print $2}'
+curl -I -L -s -X OPTIONS $1 | grep -i 'allow' | sed 's/allow: //I'
