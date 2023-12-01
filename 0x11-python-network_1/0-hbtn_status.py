9598406eac96cd80196b23b8d39938dc89015546
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-01 22:06:32 +0000

    Python Network Mandatory task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,7 @@
 """ Script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
-with urllib.request.urlopen(request) as url:
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as url:
 
     content = url.read()
     print("""Body response:
