ad16e0d17f40788ccfca8ef4cf254c5437d72eb3
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-01 23:17:49 +0000

    Python Network Mandatory task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Sends a POST request to the passed URL with the email as a parameter """
 import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == '__main__':
-    email = sys.argv[2]
+    email = urllib.parse.urlencode({"email": sys.argv[2]}).encode('utf-8')
+    
     obj = urllib.request.Request(sys.argv[1], data=email, method='POST')
 
     with urllib.request.urlopen(obj) as url:
