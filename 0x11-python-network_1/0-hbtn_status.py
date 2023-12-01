f751bdab3c19c8fa2c7fc8ab8e5261f5cbea4ca2
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-01 22:04:01 +0000

    Python Network Mandatory task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,11 @@ import urllib.request
 request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 
 with urllib.request.urlopen(request) as url:
+
+    content = url.read()
     print("""Body response:
     \t- type: {}
     \t- content: {}
-    \t- utf8 content: {}""".format(type(url), url.read(), 'OK'))
+    \t- utf8 content: {}""".format(type(content),
+                                   content,
+                                   content.decode('utf-8')))
