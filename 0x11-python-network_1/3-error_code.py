349c7b44fc5fce1c93bf219c7d349330007d69a2
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-02 01:24:56 +0000

    Python Network Mandatory task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Sends a POST request to the passed URL with the email as a parameter """
+""" Sends a request to the URL and displays the body of the response """
 import urllib.request
 import urllib.error
 import sys
