7fcf1ac1ab054c5ca3733860516b1c9924e3e5bf
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-02 16:04:29 +0000

    Python Network Advanced task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
 
     http_json = http_res.json()
 
+    index = 0
     for commit in http_json:
-        print("{}: {}".format(commit.get('sha'),
-                              commit['commit']['author']['name']))
+        if (index < 10):
+            print("{}: {}".format(commit.get('sha'),
+                                commit['commit']['author']['name']))
+        index += 1
