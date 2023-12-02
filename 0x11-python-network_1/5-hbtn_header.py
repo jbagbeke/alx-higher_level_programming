361f7c026f24e56294f2ee281c4b212d34ff689a
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-02 01:33:22 +0000

    Python Network Mandatory task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Sends request to URL and displays value of var X-Request-Id """
 import requests
+import sys
 
 if __name__ == '__main__':
-    http_res = requests.get('https://alx-intranet.hbtn.io/status')
+    http_res = requests.get(sys.argv[1])
 
     print(http_res.headers['X-Request-Id'])
