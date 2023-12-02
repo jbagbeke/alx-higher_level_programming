e1b6a2171af71c6a0a344aa16b48e632030d5dac
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-02 02:10:32 +0000

    Python Network Mandatory task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Sends a request to URL and displays body of the response """
+import requests
+import sys
+
+if __name__ == '__main__':
+    try:
+        http_res = requests.get(sys.argv[1])
+        http_res.raise_for_status()
+    except requests.exceptions.HTTPError:
+        if http_res.status_code >= 400:
+            print('Error code: {}'.format(http_res.status_code))
