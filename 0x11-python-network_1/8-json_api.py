0fd47b8e235bbdc23892c54fafe887eef91e8825
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-02 02:38:17 +0000

    Python Network Mandatory task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Sends a POST request to url with the letter as a parameter """
+import sys
+import requests
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    http_res = requests.post('http://0.0.0.0:5000/search_user', data=q)
+    content = http_res.headers.get('Content-Type', "").lower()
+
+    if "application/json" in content:
+        http_json = http_res.json()
+
+        print('{} {}'.format(http_json['id'], http_json['name']))
+    else if len(content) == 0:
+        print('No result')
+    else:
+        print('Not a valid JSON')
