83792e14312b5ffc18abb6f0cefa5e8ce822554c
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-03 21:59:20 +0000

    Python Network Advanced task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends JSON POST request to URL and displays body of response
-curl -X POST -H "Content-Type: application/json" -d @$2 $1
+curl -X POST -H "Content-Type: application/json" -d @"$2" $1
