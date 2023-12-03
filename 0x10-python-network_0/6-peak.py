a2945d11e0f224257c1a1721cbf44e6cb6ef5c1f
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-03 20:39:06 +0000

    Python Network mandatory task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,22 +5,4 @@
 def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers """
 
-    llen = len(list_of_integers)
-    list_int = list_of_integers
-    peak_list = []
-
-    for idx in range(llen):
-        if idx - 1 < 0 and idx + 1 < llen:
-            if list_int[idx] >= list_int[idx + 1]:
-                peak_list.append(list_int[idx])
-        if idx - 1 > 0 and idx + 1 >= llen:
-            if list_int[idx] >= list_int[idx - 1]:
-                peak_list.append(list_int[idx])
-        if idx - 1 > 0 and idx + 1 < llen:
-            num = list_int[idx]
-            if num >= list_int[idx - 1] and num >= list_int[idx + 1]:
-                if num not in peak_list:
-                    peak_list.append(num)
-
-    if peak_list:
-        return max(peak_list)
+    return max(peak_list)
