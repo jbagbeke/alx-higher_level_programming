a5c10128da2c7e80a2d345826149da5247aa8979
Joshua Bubune <joshua.legithive@gmail.com>
2023-12-03 21:13:52 +0000

    Python Network mandatory task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,4 +5,35 @@
 def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers """
 
-    return max(peak_list)
+    llen = len(list_of_integers)
+    llist = list_of_integers
+    peak_list = []
+
+    for idx in range(0, llen, 2):
+        if idx - 1 < 0 and idx + 1 < llen:
+            tmp = []
+            if llist[idx] not in peak_list:
+                tmp.extend([llist[idx], llist[idx + 1]])
+                peak_list.append(max(tmp))
+        if idx + 1 >= llen and idx - 1 > 0:
+            tmp = []
+            if llist[idx] not in peak_list:
+                tmp.extend([llist[idx], llist[idx - 1]])
+                peak_list.append(max(tmp))
+        if idx - 1 > 0 and idx + 1 < llen:
+            tmp = []
+            tmp.extend([llist[idx], llist[idx - 1], llist[idx + 1]])
+            if max(tmp) == llist[idx]:
+                peak_list.append(llist[idx])
+
+    if peak_list:
+        return max(peak_list)
+
+
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
