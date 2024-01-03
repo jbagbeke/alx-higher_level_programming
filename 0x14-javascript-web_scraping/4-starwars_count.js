6ac886e509096f7483e26083986ec88264cc6df7
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-03 21:53:14 +0000

    0x14. JavaScript - Web scraping Mandatory Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const sysArgs = process.argv;
 
-request(sysArgs[2], (err, response, body) => {
+request(String(sysArgs[2]), (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
@@ -12,6 +12,7 @@ request(sysArgs[2], (err, response, body) => {
     const wedgeId = 'https://swapi-api.alx-tools.com/api/people/18/';
 
     for (let i = 0; i < peopleChar.length; i++) {
+      console.log(i);
       if (peopleChar[i].characters.includes(wedgeId)) {
         wedge++;
       }
