9cfda1881eed19a5ae72a6b580208b986fba6f65
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-03 17:46:50 +0000

    0x14. JavaScript - Web scraping Mandatory Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const sysArgs = process.argv;
+
+fs.writeFile(String(sysArgs[2]), String(sysArgs[3]), (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
